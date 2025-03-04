44a60d8691d9029a444f6fe64cf5d6ab3c93e0f3
Kaushal <kaushalkr8@gmail.com>
2025-03-04 18:15:02 +0530

    first commit

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,56 +1,74 @@
-import { count, eq, or } from "drizzle-orm"
+import { and, eq, or } from "drizzle-orm"
 import { db } from "../db.config.js"
 import { contacts } from "../schema.js"
 
 
 
-    export const orderController = async (req, res) => {
-        const { phoneNumber, email } = req.body
-    
-        try {
-            const existingUsers = await db.select().from(contacts).where(or(eq(contacts.email, email), eq(contacts.phoneNumber, phoneNumber)))
-    console.log({existingUsers});
-    
-            if (existingUsers?.length > 0) {
-                const existingUser = existingUsers[0];
-    
-               const secondaryUser= await db.insert(contacts).values({
-                    phoneNumber,
-                    email,
-                    linkedId: existingUser.id, 
-                    linkPrecedence: "secondary"
-                }).returning()
-console.log({secondaryUser});
+export const orderController = async (req, res) => {
+    const { phoneNumber, email } = req.body
 
-                let contact={
-"primaryContatctId": existingUser.id,
-"emails": [existingUser.email,secondaryUser[0]?.email],
-"phoneNumbers": [existingUser.phoneNumber,secondaryUser[0]?.phoneNumber],
-"secondaryContactIds": [secondaryUser[0]?.id]
-                }
-                return res.status(201).json({ 
-                    status: true, 
-                    message: "User exists, used different credential" ,
-                    contact
-                });
-                }
-    
-             
-    
-                const newUser = await db.insert(contacts).values({
-                    phoneNumber,
-                    email,
-                    linkPrecedence: "primary"
-                }).returning(); 
-        
-                return res.status(201).json({ 
-                    status: true, 
-                    message: "New User Created", 
-                    data: newUser 
-                });
-    
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ msg: "Internal Server Error" });
+    try {
+        const existingUsers = await db.select().from(contacts).where(or(eq(contacts.email, email), eq(contacts.phoneNumber, phoneNumber)))
+
+        const exactMatch = existingUsers?.find(e => e.email === email && e.phoneNumber === phoneNumber);
+        if (exactMatch) {
+            return res.status(200).json({
+                status: true,
+                message: "User already exists",
+            });
         }
+
+
+        const primaryUser = existingUsers?.find(e => e.linkPrecedence === "primary")
+
+        if (primaryUser) {
+
+         
+
+            const secondaryUser = await db.insert(contacts).values({
+                phoneNumber,
+                email,
+                linkedId: primaryUser.id,
+                linkPrecedence: "secondary"
+            }).returning()
+            console.log({ secondaryUser });
+            const allSecondaryUsers = await db.select().from(contacts).where(
+                and(eq(contacts.linkPrecedence, "secondary"), eq(contacts.linkedId, primaryUser.id),
+                    or(eq(contacts.email, primaryUser.email), eq(contacts.phoneNumber, primaryUser.phoneNumber))
+                )
+            );
+            console.log(allSecondaryUsers);
+
+
+            let contact = {
+                "primaryContatctId": primaryUser.id,
+                "emails": [primaryUser.email, secondaryUser[0]?.email],
+                "phoneNumbers": [primaryUser.phoneNumber, secondaryUser[0]?.phoneNumber],
+                "secondaryContactIds": [secondaryUser[0]?.id]
+            }
+            return res.status(201).json({
+                status: true,
+                message: "User exists, used different credential",
+                contact
+            });
+        }
+
+
+
+        const newUser = await db.insert(contacts).values({
+            phoneNumber,
+            email,
+            linkPrecedence: "primary"
+        }).returning();
+
+        return res.status(201).json({
+            status: true,
+            message: "New User Created",
+            data: newUser
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Internal Server Error" });
     }
+}
